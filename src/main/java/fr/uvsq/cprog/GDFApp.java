f2ecd6e6a70725bd0f870dca6ab47d3fdce04a90
uvsq22306945 <a.mohamedgalab@gmail.com>
2023-11-19 14:29:59 +0100

    Mise à jour de la structure du projet

---
+++ src/main/java/fr/uvsq/cprog/GDFApp.java
@@ -0,0 +1,15 @@
+package fr.uvsq.cprog;
+
+/**
+ * La Class GDFApp représente l'interface 
+ * utilisateur de base et gère l'interaction 
+ * entre l'utilisateur et le gestionnaire de fichiers
+ *
+ */
+public class GDFApp {
+    
+    public static void main(String[] args)
+    {
+        
+    }
+}
---
+++ src/test/java/fr/uvsq/cprog/GDFAppTest.java
@@ -0,0 +1,21 @@
+package fr.uvsq.cprog;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GDFAppTest 
+{  
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+}
